b87eb8ff56b2f073bce5948e1d6b23eaaacb3097
hong-sh <rkskekfk2020@naver.com>
2021-07-07 20:19:51 +0900

    new clustering commit

---
+++ week1/news_clustering.py
@@ -0,0 +1,33 @@
+from collections import Counter
+import re
+
+def compute_jacard(set1:list, set2:list):
+    intersection = list((Counter(set1) & Counter(set2)).elements())
+    union = list((Counter(set1) | Counter(set2)).elements())
+
+    jacard = 1
+    if len(intersection) != 0 or len(union) != 0:
+        jacard = len(intersection) / len(union)
+
+    return jacard
+
+if __name__ == "__main__":
+    str1 = input().lower()
+    str2 = input().lower()
+
+    str1_set = []
+    str2_set = []
+
+    for i in range(0, len(str1)):
+        subset = str1[i:i+2]
+        if re.match("[a-z]{2}", subset) and len(subset) == 2:
+            str1_set.append(subset)
+
+    for i in range(0, len(str2)):
+        subset = str2[i:i+2]
+        if re.match("[a-z]{2}", subset) and len(subset) == 2:
+            str2_set.append(subset)
+
+    jacard = compute_jacard(str1_set, str2_set)
+
+    print(int(jacard * 65536))
