2ef84c7744ba40eae55bed5e90f580012ca755f9
hong-sh <rkskekfk2020@naver.com>
2021-07-24 19:59:16 +0900

    auto complete commit

---
+++ week3/auto_complete.py
@@ -0,0 +1,44 @@
+'''
+https://programmers.co.kr/learn/courses/30/lessons/17685
+
+["go","gone","guild"]	7
+["abc","def","ghi","jklm"]	4
+["word","war","warrior","world"]	15
+'''
+
+def find_count(current_node):
+    count = 0
+    for _, value in current_node.items():
+        if value[0] != 1:
+            count += find_count(value[1])
+        count += value[0]
+    return count
+
+def solution(words):
+    answer = 0
+
+    dictionaries = {}
+    for word in words:
+        i = 0
+        current_node = dictionaries
+        while i < len(word):
+            if word[i] not in current_node:
+                new_node = {}
+                current_node[word[i]] = [1, new_node]
+                current_node = new_node
+            else:
+                current_node[word[i]][0] += 1
+                current_node = current_node[word[i]][1]
+            i += 1
+
+    answer = find_count(dictionaries)
+    
+    return answer
+
+
+
+if __name__ == "__main__":
+    words = ["go","gone","guild"]
+    # words = ["abc","def","ghi","jklm"]
+    # words = ["word","war","warrior","world"]
+    print(solution(words))
