d31189e28d6728017a5e2e8255ca2c75204c2c27
hong-sh <rkskekfk2020@naver.com>
2021-08-03 21:11:10 +0900

    food live commit

---
+++ week4/food_live.py
@@ -0,0 +1,36 @@
+'''
+https://programmers.co.kr/learn/courses/30/lessons/42891
+
+food_times	k	result
+[3, 1, 2]	5	1
+'''
+
+def solution(food_times, k):
+    answer = 0
+    for i in range(len(food_times)):
+        food_times[i] = [food_times[i], i+1]
+    
+    food_times.sort(key=lambda x: x[0])
+
+    food_idx = 0
+    pre_sec = 0
+    sec = food_times[food_idx][0] * len(food_times[food_idx:])
+    while sec <= k:
+        food_idx += 1
+        if food_idx == len(food_times):
+            return -1
+        pre_sec = sec
+        sec += (food_times[food_idx][0] - food_times[food_idx-1][0]) * len(food_times[food_idx:])
+
+    sub_food_times = sorted(food_times[food_idx:], key=lambda x: x[1])
+    time_idx = (k-pre_sec) % len(sub_food_times)
+    answer = sub_food_times[time_idx][1]
+
+    return answer
+
+if __name__ == "__main__":
+    food_times = [3, 1, 2]
+    food_times = [4, 2, 3, 7]
+    k = 5
+
+    print(solution(food_times, k))
